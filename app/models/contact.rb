7a4646621acb02e5971388e816995ee95ff1f1ba
Paramjit Saha <sahaparamjit@gmail.com>
2017-03-24 19:01:49 +0000

    Added the rules in form validation and routes of contact form

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,5 +1,6 @@
 class Contact < ActiveRecord::Base
     validates :name, presence: true
-    validates :email, presence: true
+    validates :email, presence: true 
+    validates_format_of :email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     validates :comments, presence: true
 end
