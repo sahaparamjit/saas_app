f1d29c2b4b648d563a435c3aed38c410a47cae8b
Paramjit Saha <sahaparamjit@gmail.com>
2017-03-27 21:56:04 +0000

    Updated flash message styles

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
-  devise_for :users, controllers: 'users/registrations'
+  devise_for :users, controllers: { resgistrations: 'users/registrations' }
   get 'about', to: 'pages#about', as: 'aboutus'
   resources :contacts, only: :create
   get 'contact-us', to: 'contacts#new', as: 'new_contact'
