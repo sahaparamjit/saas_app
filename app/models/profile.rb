270b83efcc814583d570f146edf27260c88f4214
Paramjit Saha <sahaparamjit@gmail.com>
2017-03-30 08:19:49 +0000

    Added the profile create action in the controller

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -1,3 +1,5 @@
 class Profile < ActiveRecord::Base
     belongs_to :user
+    validates_format_of :contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+    #validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
 end
