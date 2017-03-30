981be9e0027b77541c4799195a68327e7c5d4d94
Paramjit Saha <sahaparamjit@gmail.com>
2017-03-30 11:34:53 +0000

    Added avatar to the profiles table

--- app/models/profile.rb
+++ app/models/profile.rb
@@ -2,4 +2,12 @@ class Profile < ActiveRecord::Base
     belongs_to :user
     validates_format_of :contact_email, :with => /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
     #validates :phone_number, format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+    
+    ## Association of paperclip for image upload on to the server.
+    has_attached_file :avatar,
+                      :styles => {:medium => "300x300", :thumb => "100x100"},
+                      :default_url => "/images/:style/missing.png"
+    validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+    
+    
 end
